e4836f430b5c14244f2b073294db7735b364b78d
Ghulam Abbas <ghulam.abbas@brainxtech.com>
2018-06-01 18:24:15 +0500

    Added reviewed and completed scrapper for coinchecker website

--- coin_scraping.py
+++ coin_scraping.py
@@ -3,8 +3,8 @@ import utilites
 import urllib.request
 import os, json
 
-coinsApi = "https://coincheckup.com/data/prod/201805292233/coins.json"
-sitName = "https://coincheckup.com/"
+coins_api = "https://coincheckup.com/data/prod/201805292233/coins.json"
+site_name = "https://coincheckup.com/"
 
 try:
     class AppURLopener(urllib.request.FancyURLopener):
@@ -13,8 +13,14 @@ try:
 
     #Getting Response from coins API
     utilites.printProcessMsg("Fetching data from coins API response")
-    coins_api_response = app_url_opener.open(coinsApi)
+    coins_api_response = app_url_opener.open(coins_api)
     utilites.printSuccessMsg("Successfully got response from coins API")
+
+    #Create folders if not exists
+    if not os.path.exists("images"):
+            os.makedirs("images")
+    if not os.path.exists("whitepapers"):
+            os.makedirs("whitepapers")
 
     #Load JSON
     data = json.load(coins_api_response)
@@ -29,35 +35,50 @@ else:
     coin_resource_api_start_url = "https://coincheckup.com/data/prod/201805292233/assets/"
 
     #Define appropriate variables
-    coin_id = coin_name = coin_symbol = coin_img = website_url = whitepaper_url = "" 
+    coin_id = coin_name = coin_symbol = coin_img_url = coin_img_name = website_url = whitepaper_url = whitepaper_name = "" 
 
     #Retrieve and store data to data dictionary
-    for element in data[:5]:
-        coin_resource_api = coin_resource_api_start_url + element["id"] + ".json"
-        print("Fetching Record for " + element["name"] + ".....")
+    for coin in data[:5]:
+        coin_resource_api = coin_resource_api_start_url + coin["id"] + ".json"
+        print("Fetching Record for " + coin["name"] + ".....")
         coin_rs_api_response = app_url_opener.open(coin_resource_api)
         coin_resource = json.load(coin_rs_api_response)
 
-        coin_id = "" if  (element["id"] == None) else element["id"]
+        if (coin["id"] != None):
+            coin_id = coin["id"]
 
-        coin_name = "" if (element["name"] == None) else element["name"]
+        if (coin["name"] != None):
+            coin_name = coin["name"]
 
-        coin_symbol = "" if (element["symbol"] == None) else element["symbol"]
+        if (coin["symbol"] != None):
+            coin_symbol = coin["symbol"]
 
-        coin_img = "" if (coin_id == "" or coin_resource["logos"]["logo"] == None) else sitName + "images/coins/" + element["id"] + "-" + coin_resource["logos"]["logo"] + ".png"
+
+        if (coin_id != "" and coin_resource["logos"]["logo"] != None):
+             coin_img_url = site_name + "images/coins/" + coin_id + "-" + coin_resource["logos"]["logo"] + ".png"    
+        if (coin_img_url != ""):
+                coin_img_name = coin_id + ".png"
+
         
-        website_url = "" if (coin_resource["research"]["website_url"] == None) else coin_resource["research"]["website_url"]
-        
-        whitepaper_url = "" if (coin_resource["research"]["whitepaper_url"] == None) else coin_resource["research"]["whitepaper_url"]
-        
+        if (coin_resource["research"]["website_url"] != None):
+            website_url = coin_resource["research"]["website_url"]
+
+        if (coin_resource["research"]["whitepaper_url"] != None):
+            whitepaper_url = coin_resource["research"]["whitepaper_url"]
+        if (whitepaper_url != None and whitepaper_url != "n/a"):
+            whitepaper_name  = coin_id + ".pdf"
+
+
         #Populate Coin
         coin_info = {
-        "id": coin_id,
-        "name": coin_name,
-        "symbol": coin_symbol,
-        "img" : coin_img,
+        "coin_id": coin_id,
+        "coin_name": coin_name,
+        "coin_symbol": coin_symbol,
+        "coin_img_url" : coin_img_url,
+        "coin_img_name" : coin_img_name,
         "website_url" : website_url,
         "whitepaper_url" : whitepaper_url,
+        "whitepaper_name" : whitepaper_name,
         }
 
         #Add Coin to dictionary of coins(market_overview)
--- utilites.py
+++ utilites.py
@@ -1,9 +1,9 @@
-from urllib.request import urlopen
 import csv, os, requests, urllib.request
+import wget
 
 def write_to_CSV(market_overview):
     """Write into CSV file"""
-    with open('coinsInformation.csv', 'w') as csvfile:
+    with open('coins_information.csv', 'w') as csvfile:
         fieldnames = ['Coin ID', 'Coin Name', 'Coin Symbol', 
         'Coin Image URL', 'Image Name(Local)', 'Coin Website', 
         'Coin Whitepaper', 'Whitepaper Name']
@@ -11,33 +11,36 @@ def write_to_CSV(market_overview):
 
         writer.writeheader()
         for coin in market_overview:
-            imageName = download_photo(coin)
-            whitepaperName = download_whitepaper(coin)
-            writer.writerow({'Coin ID': coin["id"], 'Coin Name': coin["name"],
-            'Coin Symbol' : coin["symbol"], 'Coin Image URL' : coin["img"], 
-            'Image Name(Local)': imageName, 'Coin Website' : coin["website_url"],
-            'Coin Whitepaper' : coin["whitepaper_url"], 'Whitepaper Name' : whitepaperName })
+            download_photo(coin)
+            download_whitepaper(coin)
+            writer.writerow({'Coin ID': coin["coin_id"], 'Coin Name': coin["coin_name"],
+            'Coin Symbol' : coin["coin_symbol"], 'Coin Image URL' : coin["coin_img_url"], 
+            'Image Name(Local)': coin["coin_img_name"], 'Coin Website' : coin["website_url"],
+            'Coin Whitepaper' : coin["whitepaper_url"], 'Whitepaper Name' : coin["whitepaper_name"] })
 
 def download_photo(coin):
-    if not os.path.exists("images"):
-        os.makedirs("images")
-    imageName = coin["id"] + ".png"
-    imagePath = "images/" + imageName
-    f = open(imagePath, 'wb')
-    f.write(requests.get(coin["img"]).content)
-    f.close()
-    return imageName
+    if (coin["coin_img_url"] != None):
+        imageName = coin["coin_img_name"]
+        imagePath = "images/" + imageName
+        f = open(imagePath, 'wb')
+        f.write(requests.get(coin["coin_img_url"]).content)
+        f.close()
+        return imageName
+    else:
+        return ""
 
 def download_whitepaper(coin):
+    whitepaper_url = coin["whitepaper_url"]
     if (coin["whitepaper_url"] != None or coin["whitepaper_url"] != ""):
-        if not os.path.exists("whitepapers"):
-            os.makedirs("whitepapers")
-        whitepaper_name = coin["id"] + ".pdf"
+        whitepaper_name = coin["whitepaper_name"]
         white_paper_path = "whitepapers/" + whitepaper_name
-        f = open(white_paper_path, 'wb')
-        f.write(requests.get(coin["whitepaper_url"]).content)
-        f.close()
-        return whitepaper_name            
+        response = requests.get(whitepaper_url, allow_redirects=True)
+        isPDF = response.headers.get('content-type')
+        if "pdf" in isPDF:
+            f = open(white_paper_path, 'wb')
+            f.write(requests.get(coin["whitepaper_url"]).content)
+            f.close()
+            return whitepaper_name
     else:
         return ""
 
